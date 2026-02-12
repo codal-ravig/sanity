158f9732d9ee5460cfa2a8be7d912e151175fba8
Ravi Gupta <rgupta@codal.com>
2026-02-12 13:41:17 +0530

    first commit

---
+++ apps/studio/schemaTypes/index.ts
@@ -0,0 +1,5 @@
+import { artistType } from "./artistType";
+import { eventType } from "./eventsType";
+import { venueType } from "./venueType";
+
+export const schemaTypes = [eventType , artistType, venueType]
